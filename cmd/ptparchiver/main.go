f26be67e1ead2d019986b799b313f982bb443b9c
s0up4200 <s0up4200@pm.me>
2025-01-08 13:30:12 +0100

    add debugging to test

--- cmd/ptparchiver/main.go
+++ cmd/ptparchiver/main.go
@@ -34,6 +34,7 @@ func getVersion() string {
 
 func getCommit() string {
 	if info, ok := runtime.ReadBuildInfo(); ok {
+		log.Debug().Interface("settings", info.Settings).Msg("build info settings")
 		for _, setting := range info.Settings {
 			if setting.Key == "vcs.revision" {
 				return setting.Value[:7]
